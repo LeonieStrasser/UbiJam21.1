5eb939478eb2c147cfafc4ccb0cd887b24ab03d6
LeonieStrasser <leonie.strasser@yahoo.com>
2021-10-24 23:57:40 +0200

    Bugfix
    Retry ist möglich

--- BasiliscProject/Assets/Scripts/Gamemanager.cs
+++ BasiliscProject/Assets/Scripts/Gamemanager.cs
@@ -83,6 +83,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        Time.timeScale = 1;
     }
 
     void Start()
--- BasiliscProject/Assets/Scripts/MenuManager.cs
+++ BasiliscProject/Assets/Scripts/MenuManager.cs
@@ -15,4 +15,9 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+
 }
